23bc7e485e57c035a90c3031a9ddad098c00eba7
Matías Pizarro <matias.pizarro.o96@gmail.com>
2026-07-26 19:50:47 +0200

    Create endpoint to return artist discography

--- lib/xof_music_api_web/router.ex
+++ lib/xof_music_api_web/router.ex
@@ -17,6 +17,24 @@ defmodule XofMusicApiWeb.Router do
     })
   end
 
+  get "/artists/:artist_name/discography" do
+    case XofMusicApi.Music.get_discography(artist_name) do
+      {:ok, discography} ->
+        json(conn, 200, discography)
+
+      {:error, :artist_not_found} ->
+        json(conn, 404, %{
+          error: "Artist not found"
+        })
+
+      {:error, reason} ->
+        json(conn, 500, %{
+          error: "Unable to retrieve discography",
+          reason: inspect(reason)
+        })
+    end
+  end
+
   match _ do
     json(conn, 404, %{
       error: "not_found",
