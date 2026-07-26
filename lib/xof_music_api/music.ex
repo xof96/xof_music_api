69b57bbd588c7779898a77de797684fac267d6e2
Matías Pizarro <matias.pizarro.o96@gmail.com>
2026-07-26 19:18:36 +0200

    Finish orchestrator main functions

---
+++ lib/xof_music_api/music.ex
@@ -0,0 +1,28 @@
+defmodule XofMusicApi.Music do
+  alias XofMusicApi.DeezerClient
+  alias XofMusicApi.Music.Repository
+
+  def get_discography(artist_name) when is_binary(artist_name) do
+    case Repository.get_artist_with_albums(artist_name) do
+      nil ->
+        fetch_and_store_discography(artist_name)
+
+      artist ->
+        {:ok, artist}
+    end
+  end
+
+  defp fetch_and_store_discography(artist_name) do
+    with {:ok, discography} <- DeezerClient.get_discography(artist_name) do
+      artist_attrs = %{
+        name: discography.name,
+        deezer_id: discography.deezer_id
+      }
+
+      Repository.create_artist_with_albums(
+        artist_attrs,
+        discography.albums
+      )
+    end
+  end
+end
